a8f6ffe5d5ed28780ee8e800b80aa40e2e63a979
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2019-09-14 17:13:12 -0700

    v0.0.13, transitive dependencies

--- setup.py
+++ setup.py
@@ -2,17 +2,15 @@
 Setup
 '''
 import setuptools
-
-with open('README.md', 'r') as fh:
-	LONG_DESCRIPTION = fh.read()
+import pipfile
 
 setuptools.setup(
 	name='qcrit',
-	version='0.0.12',
+	version='0.0.13',
 	author='Tim Gianitsos',
 	author_email='contact@qcrit.org',
 	description='Quantitative Criticism Lab',
-	long_description=LONG_DESCRIPTION,
+	long_description=open('README.md', 'r').read(),
 	long_description_content_type='text/markdown',
 	url='https://www.qcrit.org',
 	project_urls={'Source Code': 'https://github.com/QuantitativeCriticismLab/qcrit'},
@@ -21,5 +19,9 @@ setuptools.setup(
 		'Programming Language :: Python :: 3.6',
 		'License :: OSI Approved :: MIT License',
 		'Operating System :: OS Independent',
+	],
+	install_requires=[
+		pkg + (version if version != '*' else '')
+		for pkg, version in pipfile.load('./Pipfile').data['default'].items()
 	],
 )
